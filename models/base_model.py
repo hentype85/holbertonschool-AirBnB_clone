63774fbb4b2483780190a7642ff3fe59b62ccf3c
martin <martinleiro9@gmail.com>
2023-07-03 18:01:03 -0700

    3. BaseModel work

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""class BaseModel"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """BaseModel"""
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """return string"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+        new_dictionary = self.__dict__().copy()
+        new_dictionary["__class__"] = self.__class__.__name__
+        new_dictionary["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        new_dictionary["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        return new_dictionary

bfcf00860e30e80ca2c1b78209c2cb8bd1e8ac45
martin <martinleiro9@gmail.com>
2023-07-05 15:22:12 -0700

    4 update martin

--- models/base_model.py
+++ models/base_model.py
@@ -12,12 +12,12 @@ class BaseModel():
         strtimeFormat = "%Y-%m-%dT%H:%M:%S.%f"
         if len(kwargs) != 0:
             for k, v in kwargs.items():
+                if k == "__class__":
+                    continue
                 if k == "created_at" or k == "updated_at":
                     setattr(self, k, datetime.strptime(v, strtimeFormat))
-                elif k != '__class__':
-                    setattr(self, k, v)
                 else:
-                    self.__class__.__name__ = v
+                    setattr(self, k, v)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()

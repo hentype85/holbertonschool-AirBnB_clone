f190b55afa06e2017e4b2061b7f49f3783e5d1f7
Korchea <6334@holbertonstudents.com>
2023-07-05 13:58:11 -0700

    Task 3 everything solved

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,44 +1,64 @@
 #!/usr/bin/python3
-"""unit tests"""
-
+"""Son todos los test que se me ocurrieron para probar el base model
+"""
 import unittest
-import pep8
 from models.base_model import BaseModel
 
 
-class TestCodeFormat(unittest.TestCase):
-    """test pep8"""
-    def test_pep8_conformance(self):
-        """Test that conform to PEP8"""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(
-            ["../../models/base_model.py"])
-        self.assertEqual(result.total_errors, 1,
-                         "Found pep8 code style errors and warnings")
+class Test_Base_Model(unittest.TestCase):
+    """Son todos los test que se me ocurrieron para probar el base model
+    """
 
-    def test_pep8_conformance_test(self):
-        """Test that conform to PEP8"""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(
-            ["../../tests/test_models/test_base_model.py"])
-        self.assertEqual(result.total_errors, 1,
-                         "Found pep8 code style errors and warnings")
+    def create_class(self):
+        """create class
+        """
+        self.my_model = BaseModel()
+        self.assertTrue(hasattr(self.my_model, 'id'))
+        self.assertTrue(hasattr(self.my_model, 'created_at'))
+        self.assertTrue(hasattr(self.my_model, 'updated_at'))
 
+    def test_set_name(self):
+        """test set name
+        """
+        self.my_model.name = "My First Model"
+        self.assertAlmostEqual(self.my_model.name, "My First Model")
 
-class Test_BaseModel(unittest.TestCase):
-    "class tests_BaseModel"
+    def test_set_number(self):
+        """test set number
+        """
+        self.my_model.my_number = 89
+        self.assertAlmostEqual(self.my_model.my_number, 89)
 
-    def test_new_instance_BaseModel(self):
-        new = BaseModel()
-        self.assertIsInstance(new, BaseModel)
+    def test_types(self):
+        """test types
+        """
+        self.assertIsInstance(self.my_model.id, str)
+        self.assertIsInstance(self.my_model.created_at, datetime)
+        self.assertIsInstance(self.my_model.updated_at, datetime)
+        self.assertIsInstance(self.my_model.to_dict(), dict)
 
-    def test_attributes(self):
-        new = BaseModel()
-        self.assertEqual(type(new.id), str)
+    def test_to_dict(self):
+        """test to dict
+        """
+        self.my_model_json = self.my_model.to_dict()
+        self.assertIsInstance(self.my_model_json[created_at], str)
+        self.assertIsInstance(self.my_model_json[updated_at], str)
+        self.assertAlmostEqual(self.my_model_json[__class__], "BaseModel")
 
-    def test_class(self):
-        new = BaseModel()
-        self.assertEqual(issubclass(new.__class__, BaseModel), True)
+    def test_to_dict_2(self):
+        """test to dict_2
+        """
+        self.assertIn('id', self.my_model.to_dict())
+        self.assertIn('created_at', self.my_model.to_dict())
+        self.assertIn('updated_at', self.my_model.to_dict())
+
+    def test_save(self):
+        """test save
+        """
+        old_updated_at = self.my_model.updated_at
+        self.my_model.save()
+        new_updated_at = self.my_model.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
 
 
 if __name__ == "__main__":

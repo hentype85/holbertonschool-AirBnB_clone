aee1128b078c30f2fde37467e0f2efe2d751f5a4
Korchea <6334@holbertonstudents.com>
2023-07-06 15:29:43 -0700

    Task 5 some problems solved

--- models/base_model.py
+++ models/base_model.py
@@ -12,14 +12,14 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """initialize attributes"""
         strtimeFormat = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
         if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
                     setattr(self, k, datetime.strptime(v, strtimeFormat))
-                if k != "__class__":
+                elif k != "__class__":
                     setattr(self, k, v)
         else:
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)

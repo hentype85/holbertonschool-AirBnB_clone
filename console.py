dec898e74facc1a1363b7e7235ea92fdc2fa5f11
martin <martinleiro9@gmail.com>
2023-07-07 09:33:34 -0700

    do_show added in console.py

--- console.py
+++ console.py
@@ -3,12 +3,16 @@
 
 import cmd
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """class console"""
 
     prompt = "(hbnb) "
+    all_classes = {
+        "BaseModel": BaseModel,
+        }
 
     def do_quit(self, arg):
         """exit the program"""
@@ -35,6 +39,25 @@ class HBNBCommand(cmd.Cmd):
             print(new.id)
         except Exception:
             print("** class doesn't exist **")
+
+    def do_show(self, args):
+        """ Prints the string representation of an
+            instance based on the class name and id"""
+        if len(args) == 0:
+            print("** class name missing **")
+        else:
+            list_args_splitted = args.split(" ")
+            if list_args_splitted[0] not in self.all_classes:
+                print("** class doesn't exist **")
+            if len(list_args_splitted) < 2:
+                print("** instance id missing **")
+            else:
+                class_name = list_args_splitted[0]
+                class_id = list_args_splitted[1]
+                try:
+                    print("{}.{}".format(class_name, class_id))
+                except Exception:
+                    print("** no instance found **")
 
 
 if __name__ == "__main__":

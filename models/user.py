2970b489a8df8c1d7884137fb512effa7a52a825
martin <martinleiro9@gmail.com>
2023-07-08 05:20:52 -0700

    8. First User added first check

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class User"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""

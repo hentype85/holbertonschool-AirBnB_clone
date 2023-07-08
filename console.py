08b2f893e10a86c64997f39d1b628f1fefb4ee36
martin <martinleiro9@gmail.com>
2023-07-08 05:54:27 -0700

    9. More classes! and 10. Console 1.0 theres no tests added

--- console.py
+++ console.py
@@ -4,7 +4,6 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
-from models import storage
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place

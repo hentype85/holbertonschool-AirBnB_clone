2561300c0b62ca999e24c2ba2c7974442573cc5e
martin <martinleiro9@gmail.com>
2023-07-08 05:50:53 -0700

    9. More classes! and 10. Console 1.0 theres no tests added

--- console.py
+++ console.py
@@ -5,6 +5,11 @@ import cmd
 from models.base_model import BaseModel
 from models.user import User
 from models import storage
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -14,6 +19,11 @@ class HBNBCommand(cmd.Cmd):
     all_classes = {
         "BaseModel": BaseModel,
         "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review,
         }
 
     def do_quit(self, arg):
